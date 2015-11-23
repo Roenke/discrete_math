c6ba9855ac7ddde42347ac50f3cd00232518ebaf
Roenke <roenke54@gmail.com>
2015-11-24 01:10:57 +0300

    add right vertex cover. Add description to code tasks

--- matching-2/code/vertex_cover.cpp
+++ matching-2/code/vertex_cover.cpp
@@ -1,6 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
+#include <queue>
+
+/*
+Найдите минимальное вершинное покрытие в двудольном графе.
+
+На вход подается описание двудольного графа, в котором доли уже выделены явно. 
+Первая строка содержит три натуральных числа: 
+v1<100 — число вершин первой доли, 
+v2<100 — число вершин второй доли, 
+e≤v1<=v2 — число рёбер. Подразумевается, что первая доля состоит из вершин с 
+номерами от 0 до v1−1, вторая — из вершин с номерами от v1 до v1+v2−1. 
+Следующие e строк описывают рёбра: каждая из этих строк содержит 
+два числа: 0≤ui<v1 и v1≤wi<v1+v2, что означает, что между вершинами ui и wi есть ребро.
+
+Скопируйте описание графа из входа на выход и выведите единственную 
+дополнительную строку — список номеров вершин, составляющих минимальное вершинное покрытие. 
+Если таких покрытий несколько, выведите любое.
+*/
 int v1, v2, e;
 std::vector<int> used;
 std::vector<int> matching;
@@ -24,6 +42,26 @@ bool dfs(std::vector<std::vector<int>> const& graph, int v)
     }
 
     return false;
+}
+
+void bfs(int vertex, std::vector<uint8_t>& visited, std::vector<std::vector<int>>const& graph)
+{
+    int v;
+    std::queue<int> queue;
+    queue.push(vertex);
+    while(!queue.empty())
+    {
+        v = queue.front();
+        queue.pop();
+        visited[v] = 1;
+        for(auto const& v_ : graph[v])
+        {
+            if(visited[v_] == 0)
+            {
+                queue.push(v_);
+            }
+        }
+    }
 }
 
 int main()
@@ -62,28 +100,45 @@ int main()
         std::cout << edges[i].first << " " << edges[i].second << std::endl;
     }
 
-    int in_cover;
-    for (auto i = 0; i < matching.size(); ++i)
+    std::vector<std::vector<int>> new_graph(v);
+    std::vector<uint8_t> left_free(v1);
+    left_free.assign(v1, 1);
+    int left, right;
+    for (auto i = 0; i < e; ++i)
     {
-        if(matching[i] != -1)
+        left = edges[i].first;
+        right = edges[i].second;
+        if(matching[right - v1] == edges[i].first)
         {
-            auto left = matching[i];
-            auto added = false;
-            for(auto const& r_vertex: graph[left])
-            {
-                if(matching[r_vertex] == -1)
-                {
-                    in_cover = left;
-                    added = true;
-                    break;
-                }
-            }
+            new_graph[right].push_back(left);
+            left_free[left] = 0;
+        }
+        else
+        {
+            new_graph[left].push_back(right);
+        }
+    }
 
-            if(!added)
-            {
-                in_cover = i + v1;
-            }
-            std::cout << in_cover << " ";
+    std::vector<uint8_t> visited(v);
+    visited.assign(v, 0);
+
+    for (int i = 0; i < v1; ++i)
+    {
+        if (left_free[i] == 0 && !visited[i]) continue;
+
+        bfs(i, visited, new_graph);
+    }
+
+    int in_cover;
+    for (int i = 0; i < v; ++i)
+    {
+        if(i < v1 && visited[i] == 0)
+        {
+            std::cout << i << " ";
+        }
+        if (i >= v1 && visited[i] == 1)
+        {
+            std::cout << i << " ";
         }
     }
 
